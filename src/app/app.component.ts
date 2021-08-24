bda7658a5a1483fbdac6c26d1843af24e49cd595
ashleighchung <hichewash@gmail.com>
2021-08-23 23:38:21 -0400

    started routing fix??

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,9 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+//let displayDashboard = true;
+
 export class AppComponent {
   title = 'portfolioManagerClient';
 }
