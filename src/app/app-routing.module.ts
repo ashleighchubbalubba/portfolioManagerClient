672ee9f4065aef32e0ea4164afcbe50fe81eb3ed
ashleighchung <hichewash@gmail.com>
2021-08-25 12:38:44 -0400

    routing worksgit add ., now onto html/css for stock page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { StockPageComponent } from './stock-page/stock-page.component';
+import { WrapperComponent } from './wrapper/wrapper.component';
+
 
 const routes: Routes = [
-  {path:'stock-page', component:StockPageComponent}
+  { path: '', redirectTo: '/home-page', pathMatch: 'full'},
+  { path:'stock-page', component: StockPageComponent },
+  { path: 'home-page', component: WrapperComponent},
 ];
 
 @NgModule({
