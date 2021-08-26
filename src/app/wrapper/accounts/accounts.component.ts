c6444834287c7194f3a416e114c7617c62e6f611
ashleighchung <hichewash@gmail.com>
2021-08-25 23:29:13 -0400

    finished big values and accounts HTTPS, now onto more HTTPS

--- src/app/wrapper/accounts/accounts.component.ts
+++ src/app/wrapper/accounts/accounts.component.ts
@@ -11,30 +11,36 @@ export class AccountsComponent implements OnInit {
 
   constructor(private http: HttpClient) { 
   }
+
   allAccounts:string[]= [];
   todaysCashBanks:any = [];
   todaysInvestBanks:any = [];
   totalCashValue:number = 0;
   totalInvestValue:number = 0;
+  netWorth:number = 0;
+
   cash = "CASH";
   investment = "INVESTMENT";
 
   ngOnInit(): void {
     this.getTodaysAccountBalances();
-
   }
 
   getTodaysAccountBalances(){
+    let today = new Date();
+    let todayString = "";
+    todayString = today.getFullYear() + '-' + String(today.getMonth() + 1).padStart(2, '0') + '-' + String(today.getDate()).padStart(2, '0');
+    
     this.http.get('http://localhost:8080/getAccounts').subscribe((res:any) => {
       for (let el of res){
         // console.log(el["date"]);
         let splitObject = el["date"].split('T');
         // console.log(splitObject[0]);
         el["date"] = splitObject[0];
-        if(el["date"] == "2021-08-24" && el["type"] == "Cash"){
+        if(el["date"] == todayString && el["type"] == "Cash"){
           this.todaysCashBanks.push(el);
         }
-        else if(el["date"] == "2021-08-24" && el["type"] == "Cash"){
+        else if(el["date"] == todayString && el["type"] == "Investment"){
           this.todaysInvestBanks.push(el);
         }
       }  
@@ -45,11 +51,9 @@ export class AccountsComponent implements OnInit {
       this.totalInvestValue = this.todaysInvestBanks.reduce((total:any, elem:any) => {
         return total + elem["balance"];
       }, 0);
+      this.netWorth = this.totalCashValue + this.totalInvestValue;
       // console.log(this.todaysCashBanks); 
       //console.log(this.todaysInvestBanks)
-      })
-    }
-  
-  
-
+    })
+  }
 }
--- src/app/wrapper/dashboard/overview/overview.component.ts
+++ src/app/wrapper/dashboard/overview/overview.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-overview',
@@ -7,9 +8,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OverviewComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  allAccounts:string[]= [];
+  todaysCashBanks:any = [];
+  todaysInvestBanks:any = [];
+  totalCashValue:number = 0;
+  totalInvestValue:number = 0;
+  netWorth:number = 0;
 
   ngOnInit(): void {
+    this.getValues();
+  }
+
+  
+
+  getValues() {
+    let today = new Date();
+    let todayString = "";
+    todayString = today.getFullYear() + '-' + String(today.getMonth() + 1).padStart(2, '0') + '-' + String(today.getDate()).padStart(2, '0');
+
+    console.log(todayString);
+
+    this.http.get('http://localhost:8080/getAccounts').subscribe((res:any) => {
+      for (let el of res){
+        let splitObject = el["date"].split('T');
+        el["date"] = splitObject[0];
+
+
+        if(el["date"] == todayString && el["type"] == "Cash"){
+          this.todaysCashBanks.push(el);
+        }
+        else if(el["date"] == todayString && el["type"] == "Investment"){
+          this.todaysInvestBanks.push(el);
+        }
+      }  
+      this.totalCashValue = this.todaysCashBanks.reduce((total:any, elem:any) => {
+        return total + elem["balance"];
+      }, 0);
+      this.totalInvestValue = this.todaysInvestBanks.reduce((total:any, elem:any) => {
+        return total + elem["balance"];
+      }, 0);
+      this.netWorth = this.totalCashValue + this.totalInvestValue;
+    })
   }
 
 }
